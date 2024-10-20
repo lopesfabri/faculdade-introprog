d47fd596c37ba51c32366fd58df4333170dc17c2
Fabrício Lopes <fabriciogabriel04@gmail.com>
2024-10-20 09:38:59 +0100

    programa 2-4

---
+++ Modulo1/Capitulo2/2-4/main.c
@@ -0,0 +1,13 @@
+// Programa 2-4 Imprime diferentes tipos de variáveis
+
+#include <stdio.h>
+
+int main()
+{
+    char c = 'a';
+    float x = 12354234.2346;
+    double y = 12354234.2346;
+
+    printf("c: %c (%d), x: %f, y: %g", c, c, x, y);
+}
+
