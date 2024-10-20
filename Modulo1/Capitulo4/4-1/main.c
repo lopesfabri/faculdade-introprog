3c0c68c992bfac3501c793d06c51bb8f2a89d3ff
Fabrício Lopes <fabriciogabriel04@gmail.com>
2024-10-20 12:07:39 +0100

    programa 4-1

---
+++ Modulo1/Capitulo4/4-1/main.c
@@ -0,0 +1,15 @@
+// 4-1 Soma dos primeiros 4 quadrados naturais.
+
+#include <stdio.h>
+
+int main() {
+
+    int soma=0;
+
+    soma=soma+1*1;
+    soma=soma+2*2;
+    soma=soma+3*3;
+    soma=soma+4*4;
+
+    printf("Soma dos primeiros 4 quadrados: %d", soma);
+}
