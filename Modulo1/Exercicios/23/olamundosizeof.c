8afcb2511714feb2f504e1a68b1e32484dd46555
Fabrício Lopes <fabriciogabriel04@gmail.com>
2024-10-22 07:19:01 +0100

    ex 23 olamundosizeof

---
+++ Modulo1/Exercicios/23/olamundosizeof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Olá Mundo!\n");
+    printf("sizeof(char): %d\n", (int)sizeof(char));
+    printf("sizeof(short): %d\n", (int)sizeof(short));
+    printf("sizeof(int): %d\n", (int)sizeof(int));
+    printf("sizeof(long): %ld\n", (long)sizeof(long));
+    printf("sizeof(long long): %lld\n", (long long)sizeof(long long));
+    printf("sizeof(float): %d\n", (int)sizeof(float));
+    printf("sizeof(double): %d\n", (int)sizeof(double));
+    printf("sizeof(long double): %d\n", (int)sizeof(long double));
+}
