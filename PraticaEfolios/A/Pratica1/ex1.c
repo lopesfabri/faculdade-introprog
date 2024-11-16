f8c65fa7afa733a3b9c889e8aaa35913f909078c
Fabrício Lopes <fabriciogabriel04@gmail.com>
2024-11-16 16:06:33 +0000

    pratica1 - efolioA - ex1

---
+++ PraticaEfolios/A/Pratica1/ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char *baralho[] =
+   {
+        "+1A", "+1B", "+1C", "+1D", "+2A", "+2B", "+2C", "+2D", "+3A", "+3B", "+3C", "+3D", "+4A", "+4B", "+4C", "+4D",
+        "-1A", "-1B", "-1C", "-1D", "-2A", "-2B", "-2C", "-2D", "-3A", "-3B", "-3C", "-3D", "-4A", "-4B", "-4C", "-4D",
+        "*1A", "*1B", "*1C", "*1D", "*2A", "*2B", "*2C", "*2D", "*3A", "*3B", "*3C", "*3D", "*4A", "*4B", "*4C", "*4D",
+        "/1A", "/1B", "/1C", "/1D", "/2A", "/2B", "/2C", "/2D", "/3A", "/3B", "/3C", "/3D", "/4A", "/4B", "/4C", "/4D",
+        "\0"
+   };
+
+    int i, carta;
+    printf("Digite um numero:");
+    scanf("%d", &carta);
+
+    if(carta < 0 || carta >= 64) {
+        printf("Carta invalida\n");
+    } else {
+    printf("%s",baralho[carta]);
+    }
+}
